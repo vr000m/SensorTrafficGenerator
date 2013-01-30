661eace689dc2a142450375658a8213b8fbe28de
vr000m <varun.singh@gmail.com>
2013-01-30 15:18:11 +0200

    added writing sensor ids to sensor.list

--- iot-launcher.py
+++ iot-launcher.py
@@ -43,10 +43,17 @@ def main(argv):
   
   start_time = round(time.time(), 3)
   #[STEP1]: starting IoT Server to handle input from sensors
-  spawns.append(subprocess.Popen(["python","iot-server.py", ipaddr, port],stdout=True,shell=False))
+  # NOTE: depending on your coding language the values in the list will change
+  # Also implement SIGINT (Ctrl+C), so that we can kill them. 
+  #spawns.append(subprocess.Popen(["iot-server.o", ipaddr, port],stdout=False,shell=False))
+  spawns.append(subprocess.Popen(["python","iot-server.py", ipaddr, port],stdout=False,shell=False))
 
   #[STEP2]: starting sensors
   sensors=["device", "temp", "gps", "camera"]
+  
+  #creating sensor.list
+  sensorFile = open('sensor.list', 'wb')
+
   for i in range(N): 
     #choose randomly a sensor from the list
     sensor_type=sensors[i]
@@ -56,16 +63,24 @@ def main(argv):
       sensor_type=random.choice(sensors)
       sid=str(random.randint(10000,99999))
     
-    print "starting",i,sensor_type,"...\n"
+    print "starting",(sensor_type+sid),"..."
+    sensorFile.write(str(sensor_type+sid+"\n"))
     #forking a subprocess 
     spawns.append(subprocess.Popen(["python","sensor.py", sensor_type, ipaddr, port, sid],stdout=False,shell=False))
     #print spawns[i].pid
     #spawns[i].wait()
-
-  #[STEP3]: start your clients here
-  #append processes to spawns list. so that we can kill them. Also implement Ctrl+C 
-
-
+  
+  #close sensor.list file
+  sensorFile.close
+  
+  '''
+  [STEP3]: start your clients here
+  NOTE: depending on your coding language the values in the list will change
+  Also implement SIGINT (Ctrl+C), so that we can kill them. 
+  # for i in range(M): # where M is the number of clients. This should be added to the command line args if needed
+  #   spawns.append(subprocess.Popen(["iot-client.o","ipaddr, port, sensorid],stdout=True,shell=False))
+  '''
+  
   time.sleep(sim_time)
   print "killing processes after", (time.time()-start_time)
   for s in spawns:
